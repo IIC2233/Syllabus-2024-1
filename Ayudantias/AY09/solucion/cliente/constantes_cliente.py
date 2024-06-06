e8c024bf08d1c12ab0ac7923965528d35491a4d5
Catalina Miranda <ccmiranda1@uc.cl>
2024-06-05 22:03:33 -0400

    AY09 solucion

---
+++ Ayudantias/AY09/solucion/cliente/constantes_cliente.py
@@ -0,0 +1,6 @@
+#SERVER_URL = "http://127.0.0.1:4444"
+SERVER_URL = "https://ay09-avanzada-server.onrender.com"
+
+REGEX_NOMBRE = r'^\w+$'
+REGEX_APELLIDO = r'^\w+$'
+REGEX_N_ALUMNO = r'^\d{7}(\d|J|j)$'
